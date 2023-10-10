4b46732f9a1552c9881d4234509bfb0a96d96aff
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-10-10 16:04:45 +0900

    enhance object sample

--- src/lang/object.test.ts
+++ src/lang/object.test.ts
@@ -4,7 +4,8 @@ type PersonData = {
   sex: 'male' | 'female'
 }
 
-class Person implements PersonData {
+// Option 1
+class Person1 implements PersonData {
   name: string = ''
   age: number = 0
   sex: 'male' | 'female' = 'male'
@@ -18,14 +19,24 @@ class Person implements PersonData {
   }
 }
 
+// Option 2
+class Person2 {
+  constructor(public data: PersonData) {}
+
+  isAdult() {
+    const { age } = this.data
+    return age >= 20
+  }
+}
+
 describe('object', () => {
   test('json to object', () => {
     const data1: PersonData = { name: 'Mikasa', age: 20, sex: 'female' }
     const data2: PersonData = { name: 'Eren', age: 18, sex: 'male' }
-    const obj1 = new Person(data1)
-    const obj2 = new Person(data2)
+    const obj1 = new Person1(data1)
+    const obj2 = new Person2(data2)
     expect(obj1).toEqual(data1)
-    expect(obj2).toEqual(data2)
+    expect(obj2.data).toEqual(data2)
     expect(obj1.isAdult()).toEqual(true)
     expect(obj2.isAdult()).toEqual(false)
   })
