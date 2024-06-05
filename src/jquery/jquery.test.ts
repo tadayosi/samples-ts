78ffd4374c025a1265b5a78399beca7d6c573d07
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2024-06-05 19:08:48 +0900

    add jolokia example to jquery

--- src/jquery/jquery.test.ts
+++ src/jquery/jquery.test.ts
@@ -1,3 +1,4 @@
+import Jolokia from 'jolokia.js'
 import $ from 'jquery'
 
 describe('jQuery', () => {
@@ -6,13 +7,14 @@ describe('jQuery', () => {
     const value = '900719925474099123' // number larger than max safe integer
     $.ajax({
       url: `http://localhost:8778/jolokia/exec/samples.ts:name=Sample/longValue(long)/${value}`,
-      success: (data, _status, _xhr) => {
+      dataType: 'json',
+      success: (data, status, _xhr) => {
+        console.log('status =', status)
+        console.log('type =', typeof data)
         console.log('data =', data)
         try {
-          expect(data).toContain(value)
-          // Converting to JSON fails with value larger than max safe integer
-          //const result = JSON.parse(data)
-          //expect(result.value).toEqual(value)
+          // Number larger than max safe integer is not converted accurately
+          expect(data.value).not.toEqual(value)
           done()
         } catch (error) {
           done(error)
@@ -22,5 +24,30 @@ describe('jQuery', () => {
         done(error)
       },
     })
+  })
+
+  // To run this test, run `./src/jquery/jolokia.java` first
+  test.skip('jolokia', done => {
+    const jolokia = new Jolokia('http://localhost:8778/jolokia')
+    const value = '900719925474099123' // number larger than max safe integer
+    jolokia.request(
+      { type: 'exec', mbean: 'samples.ts:name=Sample', operation: 'longValue(long)', arguments: [value] },
+      {
+        //dataType: 'text',
+        success: resp => {
+          console.log('type =', typeof resp)
+          console.log('resp =', resp)
+          try {
+            expect(resp).toContain(value)
+            done()
+          } catch (error) {
+            done(error)
+          }
+        },
+        error: error => {
+          done(error)
+        },
+      },
+    )
   })
 })
